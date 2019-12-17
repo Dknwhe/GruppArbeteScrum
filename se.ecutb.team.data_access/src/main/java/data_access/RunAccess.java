84cc31d6643c398dacd794e433931a863bc8548d
Dknwhe <cai.mac@hotmail.com>
2019-12-17 15:05:02 +0100

    test

--- se.ecutb.team.data_access/src/main/java/data_access/RunAccess.java
+++ se.ecutb.team.data_access/src/main/java/data_access/RunAccess.java
@@ -34,5 +34,31 @@ public class RunAccess {
     public static void main(String[] args) {
         RunAccess run = new RunAccess();
         run.School();
+        System.out.println("Hejsan");
+        System.out.println("Hejsan");
+
+        System.out.println("Hejsan");
+
+        System.out.println("Hejsan");
+
+        System.out.println("Hejsan");
+        System.out.println("Hejsan");
+        System.out.println("Hejsan");
+        System.out.println("Hejsan");
+        System.out.println("Hejsan");
+        System.out.println("Hejsan");
+
+        System.out.println("Hejsan");
+
+        System.out.println("Hejsan");
+
+        System.out.println("Hejsan");
+        System.out.println("Hejsan");
+
+
+
+
+
+
     }
 }
